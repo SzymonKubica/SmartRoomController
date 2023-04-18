e6de6a835729e38914953a400f79131f7dfd8eb1
Szymon Kubica <szymo.kubica@gmail.com>
2023-04-18 09:38:42 +0200

    Added test code for the keypad and started interfacing with the RTC module.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ use core::panic::PanicInfo;
 
 extern crate ds323x;
 extern crate embedded_hal;
+extern crate arduino_hal;
 extern crate ag_lcd;
 extern crate numtoa;
 use ag_lcd::{Display, Blink, Cursor, LcdDisplay};
-use numtoa::NumToA;
+use ds323x::{Ds323x, NaiveDate, DateTimeAccess};
+use keypad::Keypad;
+mod keypad;
 
 
 #[panic_handler]
@@ -32,6 +35,17 @@ fn main() -> ! {
     let d6 = pins.d6.into_output().downgrade();
     let d7 = pins.d7.into_output().downgrade();
 
+    let mut i2c = arduino_hal::I2c::new(
+        peripherals.TWI, //
+        pins.a4.into_pull_up_input(), // use respective pins
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
+
+    let mut rtc = Ds323x::new_ds3231(i2c);
+    let datetime = NaiveDate::from_ymd_opt(2020, 5, 1).unwrap().and_hms_opt(19, 59, 58).unwrap();
+    rtc.set_datetime(&datetime).unwrap();
+
     let mut lcd: LcdDisplay<_,_> = LcdDisplay::new(rs, en, delay)
         .with_half_bus(d4, d5, d6, d7)
         .with_display(Display::On)
@@ -43,13 +57,15 @@ fn main() -> ! {
 
     let mut adc = arduino_hal::Adc::new(peripherals.ADC, Default::default());
     let a0 = pins.a0.into_analog_input(&mut adc);
+    let mut keypad = Keypad::new(a0, adc);
 
     loop {
-        let voltage = a0.analog_read(&mut adc);
-        let mut buffer = [0u8; 16];
-        let voltage: &str = voltage.numtoa_str(10, &mut buffer);
-        lcd.clear();
-        lcd.print(voltage);
+        let selection = keypad.get_input();
+        if let Some(input) = selection {
+           lcd.clear();
+           lcd.print(input.to_string());
+        }
+
         arduino_hal::delay_ms(100);
     }
    }
